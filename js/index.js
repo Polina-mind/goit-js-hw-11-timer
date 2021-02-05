7339d0bcc886612b7ffe4f39c72a00580b59c8ca
Polina-mind <popova.v.polina@gmail.com>
2021-02-05 13:36:10 +0200

    Update

--- js/index.js
+++ js/index.js
@@ -3,20 +3,29 @@ const refs = {
   hours: document.querySelector('span[data-value="hours"]'),
   mins: document.querySelector('span[data-value="mins"]'),
   secs: document.querySelector('span[data-value="secs"]'),
+  header: document.querySelector("h1"),
 };
 
-const countdownTimer = {
-  intervalId: null,
-  targetDate: new Date("Jul 15, 2021"),
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    (this.selector = selector), (this.targetDate = new Date(targetDate));
+  }
+
+  intervalId = null;
 
   start() {
+    refs.header.insertAdjacentHTML(
+      "beforeend",
+      `<p class="text">До ${this.targetDate} осталось:</p>`
+    );
+
     this.intervalId = setInterval(() => {
       const currentDate = Date.now();
       const time = this.targetDate.getTime() - currentDate;
 
       this.transfer(time);
     }, 1000);
-  },
+  }
 
   transfer(time) {
     const days = Math.floor(time / (1000 * 60 * 60 * 24));
@@ -30,41 +39,51 @@ const countdownTimer = {
     refs.hours.textContent = `${hours}:`;
     refs.mins.textContent = `${mins}:`;
     refs.secs.textContent = `${secs}`;
-  },
+  }
 
   pad(value) {
     return String(value).padStart(2, "0");
-  },
-};
+  }
+}
 
-countdownTimer.start();
+const myTimer = new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("May 15, 2021"),
+});
 
-// new CountdownTimer({
-//   selector: "#timer-1",
-//   targetDate: new Date("Jul 17, 2019"),
-// });
+myTimer.start();
 
-// let timerId = setInterval(onCountdown, 1000);
+// НЕ ЧЕРЕЗ КЛАСС
+// const countdownTimer = {
+//   intervalId: null,
+//   targetDate: new Date("Jul 15, 2021"),
 
-// function onCountdown() {
-//   setInterval(onCountdown, 1000);
-//   const currentDate = Date.now();
-//   // console.log(currentDate);
+//   start() {
+//     this.intervalId = setInterval(() => {
+//       const currentDate = Date.now();
+//       const time = this.targetDate.getTime() - currentDate;
 
-//   const targetDate = new Date("Jul 17, 2021");
-//   // console.log(targetDate.getTime());
+//       this.transfer(time);
+//     }, 1000);
+//   },
 
-//   let time = targetDate.getTime() - currentDate;
+//   transfer(time) {
+//     const days = Math.floor(time / (1000 * 60 * 60 * 24));
+//     const hours = this.pad(
+//       Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+//     );
+//     const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+//     const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-//   const days = Math.floor(time / (1000 * 60 * 60 * 24));
-//   const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-//   const secs = Math.floor((time % (1000 * 60)) / 1000);
+//     refs.days.textContent = `${days}:`;
+//     refs.hours.textContent = `${hours}:`;
+//     refs.mins.textContent = `${mins}:`;
+//     refs.secs.textContent = `${secs}`;
+//   },
 
-//   refs.days.textContent = days;
-//   refs.hours.textContent = hours;
-//   refs.mins.textContent = mins;
-//   refs.secs.textContent = secs;
-// }
+//   pad(value) {
+//     return String(value).padStart(2, "0");
+//   },
+// };
 
-//
+// countdownTimer.start();
